14d4642b3e0e382f7e6f0166de6f6d38e3236599
Patil Prasann <124381560+Prasann050997@users.noreply.github.com>
2023-04-03 00:02:01 +0530

    Add files via upload

---
+++ Assignment-5.Q-5.py
@@ -0,0 +1,41 @@
+# Challenge 5: Handling a Bank Account
+class user:
+    def _init_(self, title, balance, accountno):
+        self.title = title
+        self.balance = balance
+        self.accountno = accountno
+    def getbalance(self):
+         return self.balance
+    def deposite(self):
+         amount = float(input('enter the amount to be deposited'))
+         self.balance = self.balance + amount
+         print('deposite is successful and the balance in the account is %f' % self.balance)
+         
+
+    def withdraw(self):
+         amount = float(input('enter the amount to withdraw'))
+         self.amount = amount
+         if self.amount > self.balance:
+              print('Insufficient Funds | Balance Available: Rs',self.balance)
+         else:
+              self.balance = self.balance - self.amount
+              print('Account balance has been updated: Rs',self.balance)
+    def enquiry(self):
+         print('balance in the account is %f' % self.balance)
+    
+class SavingsAccount(user):
+    def _init_(self, title, balance, accountno, interestRate):
+          super()._init_(title, balance, accountno)
+          self.interestRate = interestRate
+    def interestAmount(self):
+         return(self.interestRate*self.Balance)/100
+name=input('Enter customer name: ')
+balance=float(input('enter your amount'))
+acc=int(input('enter your accountno'))
+itr=float(input('interestRate'))
+customer1 = SavingsAccount(name,balance,acc,itr)
+print(customer1.getbalance())
+customer1.deposite()
+print(customer1.getbalance())
+customer1.withdraw()
+print(customer1.getbalance())
